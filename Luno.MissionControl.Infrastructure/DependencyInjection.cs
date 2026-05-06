9a4c907b566b67a2202d004fd7bff6194ab76a09
hsm207 <hsm207@users.noreply.github.com>
2026-05-06 22:09:25 +0200

    refactor: establish infrastructure project and decouple luno sdk bridge from application

---
+++ Luno.MissionControl.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+using Luno.MissionControl.Application.Ports;
+using Luno.MissionControl.Infrastructure.Adapters;
+using Luno.SDK;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Luno.MissionControl.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddLunoClient(options =>
+        {
+            options.WithCredentials(
+                configuration["Luno:ApiKeyId"] ?? string.Empty,
+                configuration["Luno:ApiKeySecret"] ?? string.Empty);
+        });
+
+        services.AddScoped<LunoSdkBridge>();
+        services.AddScoped<ILunoTrader>(sp => sp.GetRequiredService<LunoSdkBridge>());
+        services.AddScoped<ILunoMarketData>(sp => sp.GetRequiredService<LunoSdkBridge>());
+
+        return services;
+    }
+}
